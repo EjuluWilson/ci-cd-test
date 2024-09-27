19d5bf0e66cf416f39d2b4b50b2daf9373af9283
Glody Bulambo <bgloire@andrew.cmu.edu>
2024-09-27 14:47:29 +0200

    get started

---
+++ __test__/App.test.tsx
@@ -0,0 +1,17 @@
+import App from '../src/App';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+describe('App shows messages', () => {
+  it('renders welcome message', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/FSE CI-CD RECITATIO/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders deploy message', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Deploy this app to render./i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import './App.css'
+
+const App:FC = () => {
+
+  return (
+    <div>
+      <h1>FSE CI-CD RECITATION</h1>
+      <p className="read-the-docs">
+        Deploy this app to render.
+      </p>
+    </div>
+  )
+}
+
+export default App
