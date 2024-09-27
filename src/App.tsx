e1509458025e5f4b85eff148ecf86c4838704d29
bgiday-cmu-F24 <bgiday@andrew.cmu.edu>
2024-09-27 16:47:08 +0200

    Add cicd pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import "./App.css";
 const App: FC = () => {
   return (
     <div>
-      <h1>FSE CI-CD RECITATION</h1>
+      <h1>This is test of CI-CD</h1>
       <p className="read-the-docs">
-        Deploy this app to render. Here we have deployed
+        Continuous Delivery (CD) and Continuous Deployment (CD) are both
+        practices within the DevOps framework aimed at streamlining the software
+        release process, but they differ in their approach to deploying code
+        changes.
       </p>
     </div>
   );
