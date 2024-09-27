9f66d6a7ba6e5689da0a7ffb29de222b845c7f77
cmnwankw-cmu-F24 <cmnwankw@andrew.cmu.edu>
2024-09-27 16:53:02 +0200

    added a failing test to verify

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 const App: FC = () => {
   return (
     <div>
-      <h1>FSE CI-CD RECITATION</h1>
+      {/* <h1>FSE CI-CD RECITATION</h1> */}
       <p className="read-the-docs">
         Deploy this app to render. Here we have deployed
       </p>
